248ea282322dedc7d54d38050cefe79a1f3fdae7
’Stephen <stephenkala@hotmail.comgit config --global user.email stephenkala@hotmail.com>
2015-07-29 17:45:26 +0000

    generating reviews controller and reviews view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 RottenMangoes::Application.routes.draw do
 
-  resources :movies
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  get 'reviews/new'
 
-end
+  get 'reviews/create'
+
+      resources :movies do
+      resources :reviews, only: [:new, :create]
+      end
+      resources :users, only: [:new, :create]
+      resources :sessions, only: [:new, :create, :destroy]
+
+    end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
